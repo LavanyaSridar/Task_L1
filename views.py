2b1020fda3e0962d361ce92cb6c6ea65ea177f38
LavanyaSridar <123250408+LavanyaSridar@users.noreply.github.com>
2023-03-09 14:04:28 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+'''
+def Studlogin(request):
+    global em,pwd
+    
+    if key=="email":
+        em=value
+    if key=="password":
+        pwd=value
+        if (em ==() and pwd==()):
+             return render(request,'error.html')
+        else:
+            return render(request,"welcome.html")
+
+            
+    return render(request, 'Studentlogin.html')
+
+def Studsignup(request):
+    return render(request, 'Studentsignup.html')
+
+def dashboard(request):
+    return render(request, 'welcome.html')
+'''
